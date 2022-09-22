ccfbf2b9759c0317587d8e28b30e43d284bea5ef
James Woolfenden <james.woolfenden@gmail.com>
2022-09-22 17:13:29 +0100

    pike

--- aws_lambda_layer_version_permission.tf
+++ aws_lambda_layer_version_permission.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_layer_version_permission" "pike" {
   layer_name     = aws_lambda_layer_version.layer.layer_name
   principal      = data.aws_caller_identity.current.account_id
   statement_id   = var.statement_id
-  version_number = aws_lambda_layer_version.pike.version
+  version_number = aws_lambda_layer_version.layer.version
 }
 
 data "aws_caller_identity" "current" {}
